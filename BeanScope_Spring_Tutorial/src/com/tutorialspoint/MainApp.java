763c1dd0abe01eefece9810d9bda74703d9a4e3c
JeffreyLehman94 <jeffreylehman94@gmail.com>
2018-02-09 21:01:12 -0500

    Add files via upload

--- BeanScope_Spring_Tutorial/src/com/tutorialspoint/MainApp.java
+++ BeanScope_Spring_Tutorial/src/com/tutorialspoint/MainApp.java
@@ -3,12 +3,23 @@ package com.tutorialspoint;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+// When Beans.xml is set to singleton, returns AAB. Prototype 
 public class MainApp {
    public static void main(String[] args) {
       ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-      obj.getMessage();
-      obj = (HelloWorld) context.getBean("helloWorld2");
-      obj.getMessage();
+      HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+
+      objA.setMessage("I'm object A");
+      
+      objA.getMessage();
+
+      HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
+      
+      
+      objB.getMessage();
+      
+      objB.setMessage("I'm object B");
+      
+      objA.getMessage();
    }
 }
